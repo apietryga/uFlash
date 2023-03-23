032e7cdccbf4a09e213c65bbabd9d38bdaed277d
apietryga <antek.pietryga@gmail.com>
2023-03-23 22:08:42 +0100

    clearing

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // init express server
 const express = require('express');
 const app = express();
-
-// init socket.io
-// const server = require('http').Server(app);
-// const io = require('socket.io')(server);
-
-// define static dir
-app.use(express.static('public'));
-
-// app.
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// });
-
-app.listen(3000, () => {
-    console.log('Server is running on port http://localhost:3000');
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => { liveReloadServer.refresh("/") }, 100);
 });
+
+app.use(connectLiveReload());
+app.use(express.static('public'));
+app.listen(3000, () => { console.log('Server is running on port http://localhost:3000') });
