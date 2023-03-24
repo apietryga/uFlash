d5a448ded4b5bd4269f3f577da5c888b301cc51c
apietryga <antek.pietryga@gmail.com>
2023-03-24 22:30:14 +0100

    workin example

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // init express server
 const express = require('express');
 const app = express();
-const livereload = require("livereload");
-const connectLiveReload = require("connect-livereload");
-const liveReloadServer = livereload.createServer();
 const api = require("./src/api.js")
 const cors = require('cors')
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => { liveReloadServer.refresh("/") }, 100);
-});
+
+// const connectLiveReload = require("connect-livereload");
+// const livereload = require("livereload");
+// const liveReloadServer = livereload.createServer();
+// liveReloadServer.server.once("connection", () => {
+//   setTimeout(() => { liveReloadServer.refresh("/") }, 100);
+// });
+// app.use(connectLiveReload());
 
 // app.use(api, '/api')
 app.use(cors())
 app.use('/api', api)
 
-app.use(connectLiveReload());
 app.use(express.static('public'));
 app.listen(3000, () => { console.log('Server is running on port http://localhost:3000') });
--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -1,14 +1,81 @@
-module.exports = new class ImageController {
-  // app.use('/api', api)
-    // capture = (req, res) => {
-    capture (req, res) {
-    // ...
+const path = require('path');
+const fs = require('fs');
 
-    res.json({
-      // ...
-      "test": true
-    })
+module.exports = new class ImageController {
+  
+  /**
+  * Getting image from external EOS camera
+  * !NOT TESTED YET! - GETTED FROM public/inc/php/functions.php
+  * @param {Object} req - Request object
+  * @param {Object} res - Response object
+  * @returns {Object} - Returns a JSON object
+  */
+  captureGET (req, res) {
+    //   $o = fopen("../bat/capture.bat","w");
+    // 	fwrite($o,getSRCdc());
+    // 	fclose($o);	
+    // 	$path = getSRC()."capture.bat";
+    // 	exec($path);
+    // /* Robienie smallPhoto */
+    // 	$width = "645px";
+    // 	$heihgt = "430px";
+    // 	$path = '../img/captured';
+    // 	$files = scandir($path, SCANDIR_SORT_DESCENDING);
+    // 	$newest_file = $files[0];
+    // 	echo $newest_file;
+    // 	$uploadedfile = $path."/".$newest_file; 
+    // 	$src = imagecreatefromjpeg($uploadedfile);        
+    // 	list($width, $height) = getimagesize($uploadedfile); 
+    // 	$tmp = imagecreatetruecolor(645, 430); 
+    // 	$filename = '../img/captured_small/small_'.$newest_file;
+    // 	imagecopyresampled($tmp, $src, 0, 0, 0, 0, 645, 430, $width, $height); 
+    // 	imagejpeg($tmp, $filename, 100);		
+    res.json({ "test": true })
   }
 
+  /**
+   * Getting image from WebCam
+   * @param {Object} req - Request object
+   * @param {Object} res - Response object
+   * @returns {Object} - Returns a JSON object
+   */
+  capturePOST (req, res) {
+    // console.log({ req })
+    const tempPath = req.file.path;
 
+    // fs create folder if not exist
+    // const dir = './uploads/';
+    const dir = path.join(__dirname, "../../storage");
+    if (!fs.existsSync(dir)){ fs.mkdirSync(dir) }
+
+
+    // const targetPath = path.join(__dirname, "./uploads/image.png");
+    const targetPath = dir + "/img_"+ fs.readdirSync(dir).length +".png";
+
+    // if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+      fs.rename(tempPath, targetPath, err => {
+        // if (err) return handleError(err, res);
+        if (err) return res.json({ err });
+        res.json({ "success": true })
+          // .status(200)
+          // .contentType("text/plain")
+          // .end("File uploaded!");
+      });
+    // } else {
+    //   fs.unlink(tempPath, err => {
+    //     if (err) return handleError(err, res);
+
+    //     res
+    //       .status(403)
+    //       .contentType("text/plain")
+    //       .end("Only .png files are allowed!");
+    //   });
+    // }
+//   }
+// );
+    
+
+   
+    // res.json({ "test": true })
+  }
 }
