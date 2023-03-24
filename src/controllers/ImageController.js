8326746c5af2c84775cc8275a0164eebace3b904
apietryga <antek.pietryga@gmail.com>
2023-03-24 21:38:15 +0100

    connect and init nodejs

---
+++ src/controllers/ImageController.js
@@ -0,0 +1,14 @@
+module.exports = new class ImageController {
+  // app.use('/api', api)
+    // capture = (req, res) => {
+    capture (req, res) {
+    // ...
+
+    res.json({
+      // ...
+      "test": true
+    })
+  }
+
+
+}
